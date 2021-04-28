6caf51111204f6ed55af81dcfaa43f937a5e6c2c
alexiy <alexiy.ov@gmail.com>
2021-04-28 21:25:41 +0300

    5 % chance to get 30 health as 'Alexiy'

--- src/main/java/dev/buildtool/scp/human/MaleCommoner.java
+++ src/main/java/dev/buildtool/scp/human/MaleCommoner.java
@@ -4,7 +4,10 @@ import dev.buildtool.scp.SCPEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
@@ -20,6 +23,12 @@ public class MaleCommoner extends Human {
     @Override
     public ILivingEntityData finalizeSpawn(IServerWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason, @Nullable ILivingEntityData spawnDataIn, @Nullable CompoundNBT dataTag) {
         entityData.set(VARIANT, random.nextInt(6));
+        if (random.nextInt(100) < 5) {
+            setCustomName(new StringTextComponent("Alexiy"));
+            setCustomNameVisible(true);
+            getAttribute(Attributes.MAX_HEALTH).addPermanentModifier(new AttributeModifier("Health bonus", 10, AttributeModifier.Operation.ADDITION));
+            heal(10);
+        }
         return super.finalizeSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
     }
 
