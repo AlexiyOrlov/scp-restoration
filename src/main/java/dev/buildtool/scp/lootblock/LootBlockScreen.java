8e419af646f8674f9630bec7da298ac1032f6c74
alexiy <alexiy.ov@gmail.com>
2021-04-30 17:38:04 +0300

    --

--- src/main/java/dev/buildtool/scp/lootblock/LootBlockScreen.java
+++ src/main/java/dev/buildtool/scp/lootblock/LootBlockScreen.java
@@ -19,7 +19,7 @@ public class LootBlockScreen extends Screen2 {
     public void init() {
         super.init();
         TextField textField = addButton(new TextField(centerX, 100, new StringTextComponent(lootBlockEntity.identifier), 150));
-        addButton(new BetterButton(centerX, 120, new StringTextComponent("Set"), p_onPress_1_ -> {
+        addButton(new BetterButton(centerX, 120, new StringTextComponent("Set identifier"), p_onPress_1_ -> {
             lootBlockEntity.identifier = textField.getValue();
             SCP.channel.sendToServer(new SetIdentifier(textField.getValue(), lootBlockEntity.getBlockPos()));
             minecraft.player.closeContainer();
