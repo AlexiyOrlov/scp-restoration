761f1405c13f315c525d293b4bf1ba2579e3fcb1
alexiy <alexiy.ov@gmail.com>
2021-05-15 21:57:40 +0300

    Limit damage to 0.9

--- src/main/java/dev/buildtool/scp/SCP.java
+++ src/main/java/dev/buildtool/scp/SCP.java
@@ -274,7 +274,7 @@ public class SCP {
             Structures.rarity = builder.comment("Higher rarity means less chambers").define("SCP chamber rarity", 240);
             writeClockworksRecipes = builder.define("Create a file listing all Clockworks recipes", true);
             toothBrushCanBreakUnbreakable = builder.define("SCP-063 can break unbreakable blocks", false);
-            chamberDamage = builder.comment("Amount of damage appplied to outer walls of generated SCP chambers").defineInRange("SCP chamber damage", 0.0, 0.0, 1.0);
+            chamberDamage = builder.comment("Amount of damage appplied to outer walls of generated SCP chambers").defineInRange("SCP chamber damage", 0.0, 0.0, 0.9);
             chaosSoldierWeight = builder.defineInRange("Spawning frequency of Chaos Insurgency soldiers", 2, 0, 20);
             return builder.build();
         }).getRight());
